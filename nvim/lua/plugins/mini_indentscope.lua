6bf34b30e073890350b3c561e69f3ed5485761ff
neilord <arshostak@gmail.com>
2024-07-27 13:31:58 +0400

    feat(nvim): change indent color

--- nvim/lua/plugins/mini_indentscope.lua
+++ nvim/lua/plugins/mini_indentscope.lua
@@ -1,34 +1,34 @@
 return {
-   "echasnovski/mini.indentscope",
+	"echasnovski/mini.indentscope",
 
-   version = false,
+	version = false,
 
-   config = function()
-      local indentscope = require("mini.indentscope")
-      indentscope.setup({
-         symbol = "▏",
-         draw = {
-            delay = 0,
-            animation = indentscope.gen_animation.none(),
-         },
-      })
+	config = function()
+		local indentscope = require("mini.indentscope")
+		indentscope.setup({
+			symbol = "▏",
+			draw = {
+				delay = 0,
+				animation = indentscope.gen_animation.none(),
+			},
+		})
 
-      vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#b0bcf2" })
 
-      vim.api.nvim_create_autocmd("BufEnter", {
-         callback = function()
-            local disabled = true
-            if
-                vim.bo.filetype ~= ""
-                and vim.bo.filetype ~= "markdown"
-                and vim.bo.filetype ~= "help"
-                and vim.bo.filetype ~= "alpha"
-                and vim.bo.filetype ~= "NvimTree"
-            then
-               disabled = false
-            end
-            vim.b.miniindentscope_disable = disabled
-         end,
-      })
-   end,
+		vim.api.nvim_create_autocmd("BufEnter", {
+			callback = function()
+				local disabled = true
+				if
+					vim.bo.filetype ~= ""
+					and vim.bo.filetype ~= "markdown"
+					and vim.bo.filetype ~= "help"
+					and vim.bo.filetype ~= "alpha"
+					and vim.bo.filetype ~= "neo-tree"
+				then
+					disabled = false
+				end
+				vim.b.miniindentscope_disable = disabled
+			end,
+		})
+	end,
 }
