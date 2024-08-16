4cdb37ffc766d09f63f13b2ddf6ff7ac34ef40c0
neilord <arshostak@gmail.com>
2024-08-16 17:20:54 +0200

    feat(nvim): use tab for codeium suggestion accept

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,12 +1,7 @@
 return {
 	"Exafunction/codeium.vim",
-	-- enabled = false,
 
 	config = function()
-		vim.keymap.set("i", "<Right>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
-
 		vim.api.nvim_set_hl(0, "CodeiumSuggestion", { link = "Comment" })
 	end,
 }
