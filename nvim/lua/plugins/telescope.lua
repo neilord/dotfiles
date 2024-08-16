a321aa86d0a280cad2e44c3db5848fe069b7fc42
neilord <arshostak@gmail.com>
2024-08-16 17:19:45 +0200

    feat(nvim): update telescope popup highlights

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,20 +1,3 @@
-local find_files = function()
-	local utils = require("telescope.utils")
-	local builtin = require("telescope.builtin")
-
-	local is_git_repo = utils.get_os_command_output({
-		"git",
-		"rev-parse",
-		"--is-inside-work-tree",
-	})[1] == "true"
-
-	if is_git_repo then
-		builtin.git_files()
-	else
-		builtin.find_files()
-	end
-end
-
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
@@ -25,54 +8,55 @@ return {
 
 	event = "VeryLazy",
 
-	config = function()
+	opts = {
+		defaults = {
+			file_ignore_patterns = {
+				"node_modules",
+				"package_lock.json",
+			},
+			mappings = {
+				n = {
+					["q"] = require("telescope.actions").close,
+				},
+			},
+
+			prompt_prefix = "   ",
+			selection_caret = " ",
+
+			sorting_strategy = "ascending",
+			layout_strategy = "vertical",
+			path_display = { "smart" },
+
+			layout_config = {
+				width = 0.6,
+				height = 0.8,
+				preview_cutoff = 40,
+				prompt_position = "top",
+			},
+		},
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+		},
+	},
+
+	config = function(_, opts)
 		local telescope = require("telescope")
-
-		telescope.setup({
-			defaults = {
-				file_ignore_patterns = {
-					"node_modules",
-					"package_lock.json",
-				},
-				mappings = {
-					n = {
-						["q"] = require("telescope.actions").close,
-					},
-				},
-
-				prompt_prefix = "   ",
-				selection_caret = " ",
-
-				sorting_strategy = "ascending",
-				layout_strategy = "vertical",
-				path_display = { "smart" },
-
-				layout_config = {
-					width = 0.6,
-					height = 0.8,
-					preview_cutoff = 40,
-					prompt_position = "top",
-				},
-			},
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
-		})
-
+		telescope.setup(opts)
 		telescope.load_extension("fzf")
 
 		vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { link = "TelescopePromptBorder" })
-		vim.api.nvim_set_hl(0, "TelescopeSelection", { link = "PmenuSel" })
+		vim.api.nvim_set_hl(0, "TelescopePromptTitle", { link = "TelescopePromptBorder" })
+		vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#9d7cd8" })
 	end,
 
 	keys = {
-		{ "<leader>f", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
-		{ "<leader>F", "<cmd>Telescope live_grep<CR>", desc = "Find Text" },
+		{ "<leader>f",  "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+		{ "<leader>F",  "<cmd>Telescope live_grep<CR>",  desc = "Find Text" },
 		{ "<leader>sh", "<cmd>Telescope highlights<CR>", desc = "Highlights" },
 	},
 }
