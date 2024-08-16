4ff4bb351397ea6557b7d650c1b4506b3c6681a4
neilord <arshostak@gmail.com>
2024-08-16 17:18:22 +0200

    fix(nvim): update colorizer filetype option

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,16 +1,17 @@
 return {
-    "NvChad/nvim-colorizer.lua",
+	"NvChad/nvim-colorizer.lua",
 
-    opts = {
-        { "*", "!lazy" },
-        {
-            user_default_options = {
-                RRGGBBAA = true,
-                AARRGGBB = true,
-                css = true,
-                tailwind = true,
-                names = false,
-            },
-        },
-    },
+	opts = {
+		filetypes = {
+			"*",
+			"!lazy",
+		},
+		user_default_options = {
+			RRGGBBAA = true,
+			AARRGGBB = true,
+			css = true,
+			tailwind = true,
+			names = false,
+		},
+	},
 }
