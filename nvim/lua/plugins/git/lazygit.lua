c416e74bfb3e255136fc92128fdd285f674dd011
neilord <arshostak@gmail.com>
2024-08-17 02:55:09 +0200

    feat(nvim): set lazygit nvim border color and size

--- nvim/lua/plugins/git/lazygit.lua
+++ nvim/lua/plugins/git/lazygit.lua
@@ -10,4 +10,9 @@ return {
 		{ "<leader>g", "<CMD>LazyGit<CR>" },
 		{ "<leader>G", "<CMD>LazyGitFilter<CR>" },
 	},
+
+	config = function()
+		vim.g.lazygit_floating_window_scaling_factor = 0.8
+		vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#709ad2" })
+	end,
 }
