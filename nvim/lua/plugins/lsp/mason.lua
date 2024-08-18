6c7d1077292cee9c3f56756c0df40eff4a42feba
neilord <arshostak@gmail.com>
2024-08-19 01:27:07 +0200

    feat(nvim): add mason keymap

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,11 +1,15 @@
 return {
-   "williamboman/mason.nvim",
+    "williamboman/mason.nvim",
 
-   opts = {
-      ui = {
-         border = 'rounded',
-         width = 0.58, -- value making it looks similar to Lazy window
-         height = 0.6,
-      },
-   },
+    opts = {
+        ui = {
+            border = "rounded",
+            width = 0.58, -- value making it looks similar to Lazy window
+            height = 0.6,
+        },
+    },
+
+    keys = {
+        { "<leader>st", "<cmd>Mason<cr>", desc = "External Tools" },
+    },
 }
